3bc3cab6c28d15ac284a3d147ba41b7735ecf5e9
MaStLucas <821190666@qq.com>
2019-03-18 23:30:04 +0800

    Test example for contentInset and contentOffset

--- IDKTableView/MasterViewController.swift
+++ IDKTableView/MasterViewController.swift
@@ -8,19 +8,25 @@
 
 import UIKit
 
-class MasterViewController: UITableViewController {
+class MasterViewController: UIViewController {
 
     var detailViewController: DetailViewController? = nil
     var objects = [Any]()
 
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
+        let contentOffsetButton = UIBarButtonItem(title: "Offset", style: .plain, target: self, action: #selector(animateContentOffset(_:)))
+        let contentInsetButton = UIBarButtonItem(title: "Inset", style: .plain, target: self, action: #selector(animateContentInset(_:)))
+        
+        let resetButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(reset(_:)))
+        navigationItem.rightBarButtonItems = [addButton, contentInsetButton, contentOffsetButton, resetButton]
+        
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -28,7 +34,7 @@ class MasterViewController: UITableViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+//        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
 
@@ -37,6 +43,24 @@ class MasterViewController: UITableViewController {
         objects.insert(NSDate(), at: 0)
         let indexPath = IndexPath(row: 0, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    @objc
+    func animateContentInset(_ sender: Any) {
+        tableView.contentInset = UIEdgeInsets.init(top: 56, left: 0, bottom: 0, right: 0)
+    }
+    
+    @objc
+    func animateContentOffset(_ sender: Any) {
+        tableView.setContentOffset(CGPoint.init(x: 0, y: -56), animated: true)
+    }
+    
+    @objc
+    func reset(_ sender: Any) {
+        tableView.contentInset = .zero
+        tableView.contentOffset = .zero
+        objects.removeAll()
+        tableView.reloadData()
     }
 
     // MARK: - Segues
@@ -53,30 +77,36 @@ class MasterViewController: UITableViewController {
         }
     }
 
-    // MARK: - Table View
+}
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+extension MasterViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.count
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         let object = objects[indexPath.row] as! NSDate
         cell.textLabel!.text = object.description
         return cell
     }
+    
+}
 
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+extension MasterViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             objects.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
@@ -84,7 +114,10 @@ class MasterViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
-
-
+    
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        print("scrollViewDidEndScrollingAnimation")
+    }
+    
 }
 
